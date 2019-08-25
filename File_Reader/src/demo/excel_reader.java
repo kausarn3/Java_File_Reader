9d25f8b9be9a7ef47c01c66a35946226918f52a3
kausarn3 <32454400+kausarn3@users.noreply.github.com>
2019-08-25 23:28:16 +0530

    Update excel_reader.java

--- File_Reader/src/demo/excel_reader.java
+++ File_Reader/src/demo/excel_reader.java
@@ -1,8 +1,8 @@
 package demo;
 
+//Download imported files from poi apache.
 import java.io.FileInputStream;
 import java.util.Iterator;
-
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.streaming.SXSSFRow.CellIterator;
@@ -13,16 +13,20 @@ public class excel_reader {
 
 	public static void main(String[] args) {
 		try {
+			//Specify file path
 			FileInputStream file = new FileInputStream("D://lecture//eclipse//a.xlsx");
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			//Index of sheet.
 			XSSFSheet sheet = workbook.getSheetAt(0);
+			//Row iterator.
 			Iterator<Row> rowIterator = sheet.iterator();
 			while (rowIterator.hasNext()) {
 				Row row = rowIterator.next();
+				//Cell iterator.
 				Iterator<Cell> cellIterator = row.cellIterator();
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
-
+					//Types of data present in excel file.
 					switch (cell.getCellType()) {
 					case STRING:
 						System.out.print(cell.getStringCellValue() + "\t");
